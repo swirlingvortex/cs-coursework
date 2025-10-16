8a5c1214110939772927602c198fd7946178e66d
Goktug Bas <08gbas@rpsnj.us>
2025-10-16 02:22:07 +0000

    added some stuff

---
+++ organize.sh
@@ -0,0 +1,4 @@
+ls -L
+chmod *cat*.jpg 660
+chmod *cat*.jpeg 660
+ls -L
---
+++ sysinfo.sh
@@ -0,0 +1,5 @@
+echo "Current Date/Time: $(date)"
+echo "Current user is $USER"
+echo "System uptime: $(uptime)"
+echo "Disk Usage of the home directory is:"
+df -h
